b046ace248d3bbae51a3783cebb60f2f367f2836
Yinjtcflm <15297927693@163.com>
2023-04-20 18:24:50 +0800

    deploy

---
+++ precache-manifest.15a4420f65f0f61baad18f05e0480425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5e74947df334acaa51",
+    "url": "/account-website-h/css/app.6f291770.css"
+  },
+  {
+    "revision": "4581908d25b859545dfd063e6e6fa366",
+    "url": "/account-website-h/index.html"
+  },
+  {
+    "revision": "aa5e74947df334acaa51",
+    "url": "/account-website-h/js/app.c7d34cf1.js"
+  },
+  {
+    "revision": "1f3ea4db4b025722ba58",
+    "url": "/account-website-h/js/chunk-vendors.e1a0a8e8.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account-website-h/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-website-h/robots.txt"
+  }
+]);
